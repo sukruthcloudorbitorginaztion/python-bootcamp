e8d6d089d019b08b8167fe17e5f7f90c925dad1b
sukruth.grandhi <sukruth@csx-group.com>
2025-10-19 08:15:30 +0530

    Remove implementation

--- python_bootcamp_cloudorbit/chapter3/floating_point_number.py
+++ python_bootcamp_cloudorbit/chapter3/floating_point_number.py
@@ -16,16 +16,7 @@ class FloatingPointNumber:
         Args:
             number (string): floating point numbers in strings. e. 123.1234567812345678
         """
-        self.sign = -1 if number[0] == "-" else 1
-        if "." not in number:
-            self.integer_part = int(number.lstrip("-"))
-            self.fraction_part = 0
-        else:
-            self.integer_part = int(number.lstrip("-").split(".")[0])
-            fraction_part_str = number.split(".")[1].rstrip("0")
-            self.fraction_part = (
-                int(number.split(".")[1].rstrip("0")) if len(fraction_part_str) else 0
-            )
+        pass
 
     def __add__(self, right):
         """
@@ -37,32 +28,7 @@ class FloatingPointNumber:
         Returns:
             [floating_point_number]: returns the added value
         """
-        right = FloatingPointNumber(str(right))
-
-        left_len = len(str(self.fraction_part))
-        right_len = len(str(right.fraction_part))
-        max_len = max(left_len, right_len)
-
-        left_fraction = 10 ** (max_len - left_len) * self.fraction_part
-        right_fraction = 10 ** (max_len - right_len) * right.fraction_part
-
-        left_number = int(f"{self.integer_part}{left_fraction}") * self.sign
-        right_number = int(f"{right.integer_part}{right_fraction}") * right.sign
-
-        total = left_number + right_number
-
-        sign = 1
-        if total < 0:
-            total = total * -1
-            sign = -1
-
-        integer_part = total // 10 ** max_len
-        fraction_part = total % 10 ** max_len
-
-        if sign == -1:
-            return FloatingPointNumber(f"-{integer_part}.{fraction_part}")
-        else:
-            return FloatingPointNumber(f"{integer_part}.{fraction_part}")
+        pass
 
     def __sub__(self, right):
         """Subtract
@@ -73,11 +39,8 @@ class FloatingPointNumber:
         Returns:
             [floating_point_number]: returns the subtracted value
         """
+        pass
 
-        temp = FloatingPointNumber(str(right))
-        temp.sign = temp.sign * -1
-
-        return self + temp
 
     def __str__(self):
         """
@@ -86,10 +49,7 @@ class FloatingPointNumber:
         Returns:
             str: returns the string format
         """
-        if self.sign == -1:
-            return f"-{self.integer_part}.{self.fraction_part}"
-        else:
-            return f"{self.integer_part}.{self.fraction_part}"
+        pass
 
     def __eq__(self, other):
         """
@@ -98,7 +58,7 @@ class FloatingPointNumber:
         Args:
             other (bool): returns either true or false
         """
-        return str(self) == str(other)
+        pass
 
     def __ne__(self, other):
         """
@@ -107,4 +67,4 @@ class FloatingPointNumber:
         Args:
             other (bool): returns either true or false
         """
-        return not (self == other)
+        pass
